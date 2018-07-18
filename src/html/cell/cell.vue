d62719cc92a62fdfb0c173494a1c835ec4a69a4f
hewenjie <hewenjie@netposa.com>
2018-07-18 10:32:03 +0800

    [修改]

--- src/html/cell/cell.vue
+++ src/html/cell/cell.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="root">
   	<div>{{message}}</div>
+  	<bar></bar>
   </div>
 </template>
 
 <script>
+import Bar from '@/components/Bar'
 export default {
   data () {
     return {
     	message: "cell"
     }
+  },
+  components: {
+  	Bar
   }
 }
 </script>
