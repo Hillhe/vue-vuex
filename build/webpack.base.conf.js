4f40881f5b64eceeee6e95bdd9b689fcb51a0417
hewenjie <hewenjie@netposa.com>
2018-07-20 10:42:17 +0800

    【修改】多页面js全部注入到index.html

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -5,98 +5,90 @@ const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 const webpack = require('webpack');
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
 }
 
 
-
 let baseConf = {
-  context: path.resolve(__dirname, '../'),
-  entry: {
-    app: './src/main.js'
-  },
-  output: {
-    path: config.build.assetsRoot,
-    filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
-  },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+    context: path.resolve(__dirname, '../'),
+    entry: {
+        app: './src/main.js'
+    },
+    output: {
+        path: config.build.assetsRoot,
+        filename: '[name].js',
+        chunkFilename: '[id].[chunkhash].[name].js',
+        publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPat : config.dev.assetsPublicPath
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': resolve('src'),
+        }
+    },
+    plugins: [],
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: vueLoaderConfig
+        }, {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        }, {
+            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 10000,
+                name: utils.assetsPath('img/[name].[hash:7].[ext]')
+            }
+        }, {
+            test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 10000,
+                name: utils.assetsPath('media/[name].[hash:7].[ext]')
+            }
+        }, {
+            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 10000,
+                name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+            }
+        }]
+    },
+    node: {
+        // prevent webpack from injecting useless setImmediate polyfill because Vue
+        // source contains it (although only uses it if it's native).
+        setImmediate: false,
+        // prevent webpack from injecting mocks to Node native modules
+        // that does not make sense for the client
+        dgram: 'empty',
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+        child_process: 'empty'
     }
-  },
-  plugins:[],
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: vueLoaderConfig
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      }
-    ]
-  },
-  node: {
-    // prevent webpack from injecting useless setImmediate polyfill because Vue
-    // source contains it (although only uses it if it's native).
-    setImmediate: false,
-    // prevent webpack from injecting mocks to Node native modules
-    // that does not make sense for the client
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
-  }
 }
 
 //为了提高编译效率，对
 if (process.argv[4] !== "spa") {
-  //获取多页面入口
-  var entriesSkill = utils.getMultiEntry('./src/html/*/index.js');
-  baseConf.entry = Object.assign(baseConf.entry, entriesSkill);
-  console.log("+++++++++++++++++++++baseConf.entry++++++++++++++++++++++");
-  console.log(baseConf.entry);
+    //获取多页面入口
+    var entriesSkill = utils.getMultiEntry('./src/html/*/index.js');
+    baseConf.entry = Object.assign(baseConf.entry, entriesSkill);
+    console.log("+++++++++++++++++++++baseConf.entry++++++++++++++++++++++");
+    console.log(baseConf.entry);
 
-  //添加多页面的公共文件提取操作
-  baseConf.plugins.push(new webpack.optimize.CommonsChunkPlugin({
-      name: ['common'],
-      chunks: Object.keys(entriesSkill),
-      minChunks: 5
-  }));
+    //添加多页面的公共文件提取操作
+    baseConf.plugins.push(new webpack.optimize.CommonsChunkPlugin({
+        name: ['common'],
+        chunks: Object.keys(entriesSkill),
+        minChunks: 5
+    }));
 }
 
 module.exports = baseConf
